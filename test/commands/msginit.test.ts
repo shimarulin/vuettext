32611c6d3a959cdbf3e26898725be78d0fe3216c
Vyacheslav Shimarulin <shimarulin@gmail.com>
2018-12-30 21:20:21 +0300

    test: add missing msginit tests

--- test/commands/msginit.test.ts
+++ test/commands/msginit.test.ts
@@ -1,17 +1,35 @@
 import {expect, test} from '@oclif/test'
+import * as del from 'del'
+import * as inquirer from 'inquirer'
 
-describe('msginit', () => {
+import {execCommand} from '../exec-command'
+
+describe('msginit', async () => {
   test
+    .do(async () => {
+      await execCommand('xgettext')
+    })
+    .stub(inquirer, 'prompt', () => Promise.resolve({languageCode: 'ru'}))
+    .stub(inquirer, 'registerPrompt', () => {
+      return true
+    })
     .stdout()
     .command(['msginit'])
-    .it('runs hello', ctx => {
-      expect(ctx.stdout).to.contain('hello world')
+    .it('runs msginit without args', async ctx => {
+      expect(ctx.stdout).to.contain('i18n/locales/ru.po.')
+
+      await del('i18n/locales')
     })
 
   test
+    .do(async () => {
+      await execCommand('xgettext')
+    })
     .stdout()
-    .command(['msginit', '--name', 'jeff'])
-    .it('runs hello --name jeff', ctx => {
-      expect(ctx.stdout).to.contain('hello jeff')
+    .command(['msginit', '--locale', 'ru_RU'])
+    .it('runs  msginit --locale=ru_RU', async ctx => {
+      expect(ctx.stdout).to.contain('i18n/locales/ru_RU.po.')
+
+      await del('i18n/locales')
     })
 })
